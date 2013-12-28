b25ab6916927223958858cb982a579f06b3f7855
Victor Zhang <zhang.victorz@gmail.com>
2013-12-28 11:28:32 -0500

    modified README

---
+++ README.md
@@ -0,0 +1,6 @@
+== README
+
+Jarvis Peanut Butter - in progress!
+
+Please feel free to use a different markup language if you do not plan to run
+<tt>rake doc:app</tt>.
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,19 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
 Pbj::Application.config.secret_key_base = 'bacbf496c71e6a9a5c541576261d13c6604078925d826807598bb89a26dcf45c9fc2c255ff55ec52af0c60452dfb603f5916a594a71ea07a11c9c6fdd7d3b52a'
