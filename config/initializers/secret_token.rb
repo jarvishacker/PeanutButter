20c9388987927e843c6a8deffdb57b13ab3355a2
Victor Zhang <zhang.victorz@gmail.com>
2013-12-28 02:03:09 -0500

    changed permissions on the lock

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pbj::Application.config.secret_key_base = 'bacbf496c71e6a9a5c541576261d13c6604078925d826807598bb89a26dcf45c9fc2c255ff55ec52af0c60452dfb603f5916a594a71ea07a11c9c6fdd7d3b52a'
