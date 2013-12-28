8fcdd527f2bc997196c15bff9014adfd069f4842
Victor Zhang <zhang.victorz@gmail.com>
2013-12-28 01:58:54 -0500

    done

--- app/models/feedback.rb
+++
@@ -1,4 +0,0 @@
-class Feedback < ActiveRecord::Base
-	belongs_to :user
-	validates :content, length: { maximum: 140 }
-end
--- app/views/feedbacks/show.json.jbuilder
+++
@@ -1,1 +0,0 @@
-json.extract! @feedback, :id, :content, :user_id, :created_at, :updated_at
